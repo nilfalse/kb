ad04270073a52aed289a577fe341e20bb62542b7
Yaroslav Ilin <nilfalse@yandex.com>
2018-04-16 22:04:46 +0200

    Add Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
-all : test.html
-.PHONY : all
+SHELL := /bin/bash
+DESTINATION = docs
+SOURCES = $(shell find . -type f -name '*.md' ! -path './node_modules/*' ! -path './README.md' -printf '%P\n')
+DOCS = $(patsubst %.md,$(DESTINATION)/%.html,$(SOURCES))
 
-test.html : test.md
-	./_engine/markdown.js test.md
+.PHONY : docs clean
+
+all : docs
+
+docs : $(DOCS) $(DESTINATION)/index.html
 
 clean :
-	rm test.html
+	- rm -f $(DOCS) $(DESTINATION)/index.html
+
+$(DESTINATION)/index.html : README.md
+$(DOCS) : $(DESTINATION)/%.html : %.md
+
+$(DESTINATION)/%.html :
+	mkdir -p $(shell dirname '$@')
+	./_engine/markdown.js < $^ > $@
--- _engine/markdown.js
+++ _engine/markdown.js
@@ -13,57 +13,14 @@ function markdown (content) {
   });
 }
 
-function parseFilename (arg) {
-  const retVal = { arg: arg, inputFilename: path.resolve(process.cwd(), arg), outputFilename: null };
-
-  switch (path.extname(arg)) {
-  case '.md':
-    retVal.outputFilename = path.resolve(process.cwd(), arg.substring(0, arg.length - 3) + '.html');
-    break;
-  default:
-    retVal.outputFilename = path.resolve(process.cwd(), arg + '.html');
-    break;
-  }
-
-  return retVal;
-}
-
-function createMarkdownStream (descriptor) {
-  const readStream = fs.createReadStream(descriptor.inputFilename, { encoding: 'utf-8' });
-  return {
-    readStream: readStream,
-    markdownStream: markdown(readStream),
-    ...descriptor
-  };
-}
-
-function streamifyMarkdownPromise (pipeline, response, idx) {
-  const descriptor = pipeline[idx];
-  return { ...descriptor, markdownStream: response.body };
-}
-
-function writeResponseToFile (descriptor) {
-  const writeStream = fs.createWriteStream(descriptor.outputFilename, { encoding: 'utf-8' });
-  descriptor.markdownStream.pipe(writeStream);
-  return new Promise((resolve, reject) => {
-    writeStream
-      .on('error', reject)
-      .on('finish', resolve);
+markdown(process.stdin)
+  .then(function (response) {
+    if (response.ok) {
+      response.body.pipe(process.stdout);
+    } else {
+      response.body.pipe(process.stderr)
+        .on('end', function () {
+          process.exit(1);
+        });
+    }
   });
-}
-
-const args = process.argv.slice(2);
-const readingPipeline = args.map(parseFilename).map(createMarkdownStream);
-
-console.log('Processing:');
-readingPipeline.forEach(descr => { console.log(descr.inputFilename); });
-
-Promise.all(readingPipeline.map(descriptor => descriptor.markdownStream))
-  .then(result => {
-    const enhancedPipeline = result
-      .map(streamifyMarkdownPromise.bind(this, readingPipeline));
-    return Promise.all(enhancedPipeline.map(writeResponseToFile))
-      .then(() => enhancedPipeline);
-  })
-  // .then(() => console.log(' ... everything went well'))
-  .catch(console.error);
